defdc65a00178d0610376f6a45f2225ae04059eb
Spareaftertaste <2383860945@qq.com>
2018-07-02 16:07:59 +0800

    测试login新增内容

--- PolyFinancial-TXJ-admin-html/js/route.js
+++ PolyFinancial-TXJ-admin-html/js/route.js
@@ -9,20 +9,6 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             controllerAs:'vm'
 
         })
-        .state('matching',{
-            url:"/matching",
-            templateUrl: 'html/matching.html',
-            controller: 'matchingCtrl',
-            controllerAs:'vm'
-
-        })
-        .state('buy',{
-            url:"/buy",
-            templateUrl: 'html/buy.html',
-            controller: 'buyCtrl',
-            controllerAs:'vm'
-
-        })
         .state('backStage',{
             url:"/backStage",
             templateUrl: 'html/backStage.html',
@@ -30,24 +16,156 @@ app.config(function ($stateProvider, $urlRouterProvider) {
             controllerAs:'vm'
 
         })
-        .state('fresh',{
-            url:"/fresh",
-            templateUrl: 'html/fresh.html',
-            controller: 'freshCtrl',
+        .state('backStage.user',{
+            url:"/user",
+            templateUrl: 'html/user.html',
+            controller: 'userCtrl',
             controllerAs:'vm'
 
         })
-        .state('main',{
-            url:"/main",
-            templateUrl: 'html/main.html',
-            controller: 'mainCtrl',
+        .state('backStage.userDetailed',{
+            url:"/userDetailed",
+            templateUrl: 'html/userDetailed.html',
+            controller: 'userDetailedCtrl',
             controllerAs:'vm'
 
         })
-        .state('main.content',{
-            url:"/content",
-            templateUrl:'html/content.html',
-            controller:'contentCtrl',
+        .state('backStage.debt',{
+            url:"/debt",
+            templateUrl: 'html/debt.html',
+            controller: 'debtCtrl',
+            controllerAs:'vm'
+
+        })
+        .state('backStage.debtEdit',{
+            url:"/debtEdit",
+            templateUrl: 'html/debtEdit.html',
+            controller: 'debtEditCtrl',
+            controllerAs:'vm'
+
+        })
+        .state('backStage.matching',{
+            url:"/matching",
+            templateUrl: 'html/matching.html',
+            controller: 'matchingCtrl',
+            controllerAs:'vm'
+
+        })
+
+        .state('backStage.product',{
+            url:"/product",
+            templateUrl: 'html/product.html',
+            controller: 'productCtrl',
+            controllerAs:'vm'
+
+        })
+        .state('backStage.productEdit',{
+            url:"/productEdit",
+            templateUrl: 'html/productEdit.html',
+            controller: 'productEditCtrl',
+            controllerAs:'vm'
+
+        })
+        .state('backStage.banner',{
+            url:"/banner",
+            templateUrl: 'html/banner.html',
+            controller: 'bannerCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.bannerEdit',{
+            url:"/bannerEdit",
+            templateUrl: 'html/bannerEdit.html',
+            controller: 'bannerEditCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.recommend',{
+            url:"/recommend",
+            templateUrl: 'html/recommend.html',
+            controller: 'recommendCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.recommendEdit',{
+            url:"/recommendEdit",
+            templateUrl: 'html/recommendEdit.html',
+            controller: 'recommendEditCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.message',{
+            url:"/message",
+            templateUrl: 'html/message.html',
+            controller: 'messageCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.messageEdit',{
+            url:"/messageEdit",
+            templateUrl: 'html/messageEdit.html',
+            controller: 'messageEditCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.messageSee',{
+            url:"/messageSee",
+            templateUrl: 'html/messageSee.html',
+            controller: 'messageSeeCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.feedback',{
+            url:"/feedback",
+            templateUrl: 'html/feedback.html',
+            controller: 'feedbackCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.feedbackEdit',{
+            url:"/feedbackEdit",
+            templateUrl: 'html/feedbackEdit.html',
+            controller: 'feedbackEditCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.feedbackSee',{
+            url:"/feedbackSee",
+            templateUrl: 'html/feedbackSee.html',
+            controller: 'feedbackSeeCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.password',{
+            url:"/password",
+            templateUrl: 'html/password.html',
+            controller: 'passwordCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.account',{
+            url:"/account",
+            templateUrl: 'html/account.html',
+            controller: 'accountCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.accountEdit',{
+            url:"/account",
+            templateUrl: 'html/accountEdit.html',
+            controller: 'accountEditCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.module',{
+            url:"/module",
+            templateUrl:'html/module.html',
+            controller:'moduleCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.moduleEdit',{
+            url:"/moduleEdit",
+            templateUrl:'html/moduleEdit.html',
+            controller:'moduleEditCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.role',{
+            url:"/role",
+            templateUrl:'html/role.html',
+            controller:'roleCtrl',
+            controllerAs:'vm'
+        })
+        .state('backStage.roleEdit',{
+            url:"/roleEdit",
+            templateUrl:'html/roleEdit.html',
+            controller:'roleEditCtrl',
             controllerAs:'vm'
         })
 });
