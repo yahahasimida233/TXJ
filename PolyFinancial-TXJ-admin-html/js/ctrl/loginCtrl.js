f0b998fe38d037da604f85ebb902596acd0250be
Spareaftertaste <2383860945@qq.com>
2018-07-04 23:10:30 +0800

    文件路径创建完成

---
+++ PolyFinancial-TXJ-admin-html/js/ctrl/loginCtrl.js
@@ -0,0 +1,3 @@
+app.controller("loginCtrl",function () {
+    var vm = this;
+});
--- PolyFinancial-TXJ-admin-html/js/route.js
+++ PolyFinancial-TXJ-admin-html/js/route.js
@@ -4,49 +4,49 @@ app.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('login',{
             url:"/login",
-            templateUrl: 'html/login.html',
+            templateUrl: 'view/html/login.html',
             controller: 'loginCtrl',
             controllerAs:'vm'
 
         })
         .state('backStage',{
             url:"/backStage",
-            templateUrl: 'html/backStage.html',
+            templateUrl: 'view/html/backStage.html',
             controller: 'backStageCtrl',
             controllerAs:'vm'
 
         })
         .state('backStage.user',{
             url:"/user",
-            templateUrl: 'html/user.html',
+            templateUrl: 'view/html/user.html',
             controller: 'userCtrl',
             controllerAs:'vm'
 
         })
         .state('backStage.userDetailed',{
             url:"/userDetailed",
-            templateUrl: 'html/userDetailed.html',
+            templateUrl: 'view/html/userDetailed.html',
             controller: 'userDetailedCtrl',
             controllerAs:'vm'
 
         })
         .state('backStage.debt',{
             url:"/debt",
-            templateUrl: 'html/debt.html',
+            templateUrl: 'view/html/debt.html',
             controller: 'debtCtrl',
             controllerAs:'vm'
 
         })
         .state('backStage.debtEdit',{
             url:"/debtEdit",
-            templateUrl: 'html/debtEdit.html',
+            templateUrl: 'view/html/debtEdit.html',
             controller: 'debtEditCtrl',
             controllerAs:'vm'
 
         })
         .state('backStage.matching',{
             url:"/matching",
-            templateUrl: 'html/matching.html',
+            templateUrl: 'view/html/matching.html',
             controller: 'matchingCtrl',
             controllerAs:'vm'
 
@@ -54,117 +54,117 @@ app.config(function ($stateProvider, $urlRouterProvider) {
 
         .state('backStage.product',{
             url:"/product",
-            templateUrl: 'html/product.html',
+            templateUrl: 'view/html/product.html',
             controller: 'productCtrl',
             controllerAs:'vm'
 
         })
         .state('backStage.productEdit',{
             url:"/productEdit",
-            templateUrl: 'html/productEdit.html',
+            templateUrl: 'view/html/productEdit.html',
             controller: 'productEditCtrl',
             controllerAs:'vm'
 
         })
         .state('backStage.banner',{
             url:"/banner",
-            templateUrl: 'html/banner.html',
+            templateUrl: 'view/html/banner.html',
             controller: 'bannerCtrl',
             controllerAs:'vm'
         })
         .state('backStage.bannerEdit',{
             url:"/bannerEdit",
-            templateUrl: 'html/bannerEdit.html',
+            templateUrl: 'view/html/bannerEdit.html',
             controller: 'bannerEditCtrl',
             controllerAs:'vm'
         })
         .state('backStage.recommend',{
             url:"/recommend",
-            templateUrl: 'html/recommend.html',
+            templateUrl: 'view/html/recommend.html',
             controller: 'recommendCtrl',
             controllerAs:'vm'
         })
         .state('backStage.recommendEdit',{
             url:"/recommendEdit",
-            templateUrl: 'html/recommendEdit.html',
+            templateUrl: 'view/html/recommendEdit.html',
             controller: 'recommendEditCtrl',
             controllerAs:'vm'
         })
         .state('backStage.message',{
             url:"/message",
-            templateUrl: 'html/message.html',
+            templateUrl: 'view/html/message.html',
             controller: 'messageCtrl',
             controllerAs:'vm'
         })
         .state('backStage.messageEdit',{
             url:"/messageEdit",
-            templateUrl: 'html/messageEdit.html',
+            templateUrl: 'view/html/messageEdit.html',
             controller: 'messageEditCtrl',
             controllerAs:'vm'
         })
         .state('backStage.messageSee',{
             url:"/messageSee",
-            templateUrl: 'html/messageSee.html',
+            templateUrl: 'view/html/messageSee.html',
             controller: 'messageSeeCtrl',
             controllerAs:'vm'
         })
         .state('backStage.feedback',{
             url:"/feedback",
-            templateUrl: 'html/feedback.html',
+            templateUrl: 'view/html/feedback.html',
             controller: 'feedbackCtrl',
             controllerAs:'vm'
         })
         .state('backStage.feedbackEdit',{
             url:"/feedbackEdit",
-            templateUrl: 'html/feedbackEdit.html',
+            templateUrl: 'view/html/feedbackEdit.html',
             controller: 'feedbackEditCtrl',
             controllerAs:'vm'
         })
         .state('backStage.feedbackSee',{
             url:"/feedbackSee",
-            templateUrl: 'html/feedbackSee.html',
+            templateUrl: 'view/html/feedbackSee.html',
             controller: 'feedbackSeeCtrl',
             controllerAs:'vm'
         })
         .state('backStage.password',{
             url:"/password",
-            templateUrl: 'html/password.html',
+            templateUrl: 'view/html/password.html',
             controller: 'passwordCtrl',
             controllerAs:'vm'
         })
         .state('backStage.account',{
             url:"/account",
-            templateUrl: 'html/account.html',
+            templateUrl: 'view/html/account.html',
             controller: 'accountCtrl',
             controllerAs:'vm'
         })
         .state('backStage.accountEdit',{
             url:"/account",
-            templateUrl: 'html/accountEdit.html',
+            templateUrl: 'view/html/accountEdit.html',
             controller: 'accountEditCtrl',
             controllerAs:'vm'
         })
         .state('backStage.module',{
             url:"/module",
-            templateUrl:'html/module.html',
+            templateUrl:'view/html/module.html',
             controller:'moduleCtrl',
             controllerAs:'vm'
         })
         .state('backStage.moduleEdit',{
             url:"/moduleEdit",
-            templateUrl:'html/moduleEdit.html',
+            templateUrl:'view/html/moduleEdit.html',
             controller:'moduleEditCtrl',
             controllerAs:'vm'
         })
         .state('backStage.role',{
             url:"/role",
-            templateUrl:'html/role.html',
+            templateUrl:'view/html/role.html',
             controller:'roleCtrl',
             controllerAs:'vm'
         })
         .state('backStage.roleEdit',{
             url:"/roleEdit",
-            templateUrl:'html/roleEdit.html',
+            templateUrl:'view/html/roleEdit.html',
             controller:'roleEditCtrl',
             controllerAs:'vm'
         })
