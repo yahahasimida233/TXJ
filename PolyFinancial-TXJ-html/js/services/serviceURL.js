6925bea7baa9c3cde9eb109525bbefeed9e6ed2f
yahaha <36730757+Spareaftertaste@users.noreply.github.com>
2018-08-05 19:45:40 +0800

    Update serviceURL.js

--- PolyFinancial-TXJ-html/js/services/serviceURL.js
+++ PolyFinancial-TXJ-html/js/services/serviceURL.js
@@ -4,48 +4,48 @@ angular.module("txj").factory("serviceURL", function() {
         loginURL: "/home/a/u/front/login/",
 
         //找回密码
-        getbackPURL:"/admin/home/a/u/my/forget",
+        getbackPURL:"/home/a/u/my/forget",
 
         //登出
         logoutURL: "/admin/a/u/logout/",
 
 
         // 普通手机短信验证码发送
-        getCodeURL: "/admin/home/a/u/forget/sendcode/",
+        getCodeURL: "/home/a/u/forget/sendcode/",
 
         //注册用手机短信验证码发送
-        verificationCodeURL: "/admin/home/a/u/sendcode/",
+        verificationCodeURL: "/home/a/u/sendcode/",
 
         // 验证注册验证码是否有效
-        codeConfirmURL: "/admin/home/a/u/verify/",
+        codeConfirmURL: "/home/a/u/verify/",
 
        
 
 
         // 提交注册信息
-        registerURL: "/home/a/u/register",
+        registerURL: "/a/u/register",
 
         //图片（伪）验证码
-        imgCodeURL: "/home/a/u/pverify/",
+        imgCodeURL: "/a/u/pverify/",
 
         //图片上传
         imgUploadURL: "/admin/a/u/picture/",
 
         //获取用户个人信息
-        userInfoURL: "/admin/home/a/u/my/info/",
+        userInfoURL: "/home/a/u/my/info/",
 
         // 实名认证第一步，提交用户名身份证号码，还有银行卡号码
-        realNameStep1URL: "/admin/home/a/u/my/info/realname/",
+        realNameStep1URL: "/home/a/u/my/info/realname/",
 
 
         // 实名认证收取验证码
-        RGetCodeURL: "/admin/home/a/u/my/info/verifypage/",
+        RGetCodeURL: "/home/a/u/my/info/verifypage/",
 
         // 实名认证验证 验证码
-        RCheckCodeURL: "/admin/home/a/u/my/info/code",
+        RCheckCodeURL: "/home/a/u/my/info/code",
 
         //提交更换手机的信息
-        newNumberURL: "/admin/home/a/u/my/info/newnum",
+        newNumberURL: "/home/a/u/my/info/newnum",
 
         //产品列表
         productListURL: "/home/a/u/productlist/",
@@ -57,25 +57,25 @@ angular.module("txj").factory("serviceURL", function() {
         personInfoURL: "/home/a/u/my/info/",
 
         //支付
-        payURL: "admin/home/a/u/pay/",
+        payURL: "/home/a/u/pay/",
 
         // 我的页面-获取消息列表
 
-        messageURL:"/admin/home/a/u/my/articles",
+        messageURL:"/home/a/u/my/articles",
 
         // 银行卡列表
-        bankCardListURL: "/admin/home/a/u/my/bankcards/",
+        bankCardListURL: "/home/a/u/my/bankcards/",
 
       
 
 
 
         // 设置-修改密码
-        settingCPURL: "/admin/home/a/u/my/setup/changepwd/",
+        settingCPURL: "/home/a/u/my/setup/changepwd/",
 
 
         // 意见反馈接口
-        feedBackURL: "/admin/home/a/u/my/suggestion"
+        feedBackURL: "/home/a/u/my/suggestion"
 
         // 理财
     };
